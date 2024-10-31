313467507720c07cb71d9f03ded4b48540b18abc
nataliodo <natalinatalova09@gmail.com>
2024-10-31 18:49:15 +0400

    commit

---
+++ Day 09/homework/homework continue.py
@@ -0,0 +1,5 @@
+""" თქვენი სიტყვებით ახსენით რა არის input და output, მოიყვანეთ მაგალითები და დაურთეთ სურათები"""
+# input = არის მაგალითად როდესაც ინფორმაცია სადმე გადაგაქვს ან შეგაქვს მაგ რამეს რომ დაწერ და ეს დაწერილი სადმე რომ გადადის 
+# output = როდესაც რამეს რომ დაწერ მაგ "visual studio code-ში და სემდეგ ტერმინალში რომ გამოაქვს ან მაგ კომპიუტერი რომ მორჩება რამის გადმოწერას ან შენახვას რაქაც ფანჯარას რომ გამოგიჩენს და ფანჯარაში ეწერება რომ გადმოწერა ან შენახვა დაასრულა"
+"https://www.shutterstock.com/image-vector/isometric-illustration-production-process-form-260nw-1925473781.jpg"
+"https://www.shutterstock.com/image-vector/isometric-illustration-production-process-input-600w-1925095406.jpg"
---
+++ Day 09/homework/homework.py
@@ -0,0 +1,6 @@
+name = input("enter your name: natali ")
+surename = input("enter your surname: odosashvili ")
+age = input("enter your age: 12 years old ")
+city = input("enter city: Tbilisi ")
+
+print("hello, " + name + " surname" + "i am " + " age" + " i live in " + " city")
