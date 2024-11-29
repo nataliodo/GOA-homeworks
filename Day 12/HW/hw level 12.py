ec10c1e768acf8993aa3d10c33bf68ab181181bb
nataliodo <natalinatalova09@gmail.com>
2024-11-29 18:02:31 +0400

    commit

---
+++ Day 12/HW/hw level 12.py
@@ -0,0 +1,27 @@
+"""2) მომხმარებელს შემოატანინეთ რიცხვი, შეინახეთ ის ცვლადში და გაამრავლეთ ორზე, შემდეგ კი დაუბეჭდეთ წინადადება ფორმატით: 'Your number multiplied by 2 is <number>' სადაც <number>-ის ნაცვლად ჩაწერეთ 2-ზე გამრავლებულ რიცხვს, გამოიყენეთ ხელოვნური მონაცემტა ტიპის შეცვლა, input-იდან მიღებული მნიშვნელობა გარდაქმენით ჯერ integer ტიპად შემდეგ კი string-ად. ახსენით კომენტარებით თუ როგორ შეასრულეთ დავალება"""
+# Here im asking user to enter his fav number (fav means favorite)
+number = input("please enter your fav number: 7 ")
+"your number multiplied by 2 is 14"
+
+"""3) მარტივი გამომთვლელი: შექმენით პროგრამა რომელიც მომხმარებელს შემოატაინინებს 2 რიცხვს და შეინახავს ცვლადებში, გარდაქმენით ეს რიცხვები integer-ად, შემდეგ კი დაბეჭდეთ მათ შორის 4 მათემატიკური ოპერაცია (+,-,*,//)"""
+
+number1 = input("please enter two number: 113 ")
+number2 = input("please enter number: 16 ")
+
+print(int(number1) + int(number2))
+print(int(number1) - int(number2))
+print(int(number1) * int(number2))
+print(int(number1) // int(number2))
+
+
+
+
+
+
+
+str("55")
+int(65)
+float(2.5)
+#str-გარდაქმნის ტექსტად
+#int- გარდაქმნის  მთელ რიცხვად
+#Float-გარდაქმნის ათწილადად
